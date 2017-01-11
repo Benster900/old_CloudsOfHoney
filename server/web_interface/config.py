1c6c61aa17f858e368fb1a2b221872b4ab69766a
root <root@cloud.localdomain>
2017-01-11 01:07:32 -0500

    Added setup script for app.vars and setup config.py to read app.vars

--- server/web_interface/config.py
+++ server/web_interface/config.py
@@ -11,11 +11,18 @@ MHN_WEB_SERVER_HOME = _basedir
 # Domain or IP address of server
 MHN_DOMAIN_NAME = 'cloud.localdomain'
 
+# Open application variables
+f = open('app.var','r')
+
 # Setup database
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://clouduser:Password123*@localhost/cloudsofhoney'
+dbUser=f.readline().strip()
+dbPass=f.readline().strip()
+dbHost=f.readline().strip()
+dbDatabase=f.readline().strip()
+SQLALCHEMY_DATABASE_URI = "mysql+pymysql://{0}:{1}@{2}/{3}".format(dbUser,dbPass,dbHost,dbHost)
 
 # Security setting
-SECRET_KEY = 'secret-key'
-SECURITY_PASSWORD_HASH = 'pbkdf2_sha256'
-SECURITY_PASSWORD_SALT = 'Iksa8RZc5a'
-SECURITY_TRACKABLE = True
+SECRET_KEY = f.readline().strip()
+SECURITY_PASSWORD_HASH = f.readline().strip()
+SECURITY_PASSWORD_SALT = f.readline().strip()
+SECURITY_TRACKABLE = False
---
+++ server/web_interface/setup.py
@@ -0,0 +1,35 @@
+#!./app/env/bin/python
+"""
+#dbUser
+#dbPass
+#dbHost
+#dbDatabase
+#SECRET_KEY
+#SECURITY_PASSWORD_HASH
+#SECURITY_PASSWORD_SALT
+"""
+import os
+import uuid
+import random, string
+from argparse import ArgumentParser
+
+def main():
+	p = ArgumentParser()
+    	p.add_argument('--dbUser', '-u', action='store', help='Username for database', default='clouduser')
+    	p.add_argument('--dbPass', '-p', action='store', help='the address to bind to', default='password123')
+    	p.add_argument('--dbHost', '-a', action='store', help='IP address of database', default='localhost')
+    	p.add_argument('--dbDatabase', '-d', action='store', help='Database to connect to', default='cloudsofhoney')
+    	p.add_argument('--dbHash', '-k', action='store', help='Hashing algorithm to use on database: plaintext, bcrypt or pbkdf2_sha256', default='pbkdf2_sha256')
+    	args = p.parse_args()
+
+	# Open file	
+	varFile = open('app.vars','w')
+	varFile.write(args.dbUser+'\n')
+	varFile.write(args.dbPass+'\n')
+	varFile.write(args.dbHost+'\n')
+	varFile.write(args.dbDatabase+'\n')
+	varFile.write(str(uuid.uuid4())+'\n')
+	varFile.write(args.dbHash+'\n')
+	varFile.write(''.join(random.choice(string.lowercase + string.uppercase + string.digits) for i in range(10)) + '\n')
+
+main()
